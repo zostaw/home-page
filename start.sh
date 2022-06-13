fdcf055574265dd68f08217b4e43a32666bbeecf
mkowalko <mateusz.kowalkowski@gmail.com>
2022-06-13 20:31:37 +0000

    added about, first draft

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 MAIN_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-
 cd $MAIN_DIR/venv/bin/
 
-ps -ef | grep "$MAIN_DIR" | grep -v grep | grep -v $! | awk '{print $2}' | xargs kill -9 &>/dev/null
+ps -ef | grep "$MAIN_DIR" | grep -v grep | grep -v start.sh | awk '{print $2}' | xargs kill -9 &>/dev/null
 
 source ./activate
 cd $MAIN_DIR
