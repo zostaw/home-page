a06c37e13ce879217964b6e38d8ee3be9bf06734
Mateusz Kowalkowski <mateusz.kowalkowski@gmail.com>
2022-09-18 16:35:51 +0200

    I added choice-menu instead of submit one in spaced repetition page. The Daily session now appears when it is picked up from menu.

---
+++ app/static/js/sr_get_records.js
@@ -0,0 +1,16 @@
+    $(document).ready(function() {
+        $('#fetch').click(function(event) {
+                let python_get_records = '/sr_get_records';
+                $.get(python_get_records, function(data) {
+                   // Get JSON data from Python script
+                   if (data){
+                      console.log("Data returned:", data);
+                   };
+                    //var parsed_datajobDataJSON = JSON.parse(data);
+                    var ddata = "<p> it ran lol "+data+".";
+                    var pdata = "<p>question: " + data["question"] + " </p><p> answer: "+data["answer"] + "</p>";
+                    console.log(ddata + "and" + pdata);
+                    $('#ds_output').html(pdata);
+                });
+        });
+    });
