b7065f6a9b6b8cef8e53e4789559ca93f97a1d51
Mateusz Kowalkowski <mateusz.kowalkowski@gmail.com>
2022-09-18 14:20:50 +0200

    1. rearanged javascript directory, it is now stored in static/js. 2. clean up spaced_repetition page, added menu - to be connected with API

--- app/static/scripts/spaced_repetition.js
+++
@@ -1,8 +0,0 @@
-//document.getElementById("space-rep-button").addEventListener("click", ()=>{spaced_rep.get_records()}, false);
-document.getElementById("space-rep-button").addEventListener("click", ()=>{document.write("asdf")}, false);
-
-document.write(5+6)
-
-space_rep.expose(prompt_alerts);
-function prompt_alerts(description) {
-  alert(description);
